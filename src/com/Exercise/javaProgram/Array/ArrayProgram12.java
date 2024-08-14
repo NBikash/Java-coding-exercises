04aefb8d8184fd79d6fffd07c748bf53631c32a4
Bikash <Bikash@DESKTOP-O6V9D41.Vapscorp.com>
2024-08-14 16:30:25 +0530

    Question updation for Program no12

--- src/com/Exercise/javaProgram/Array/ArrayProgram12.java
+++ src/com/Exercise/javaProgram/Array/ArrayProgram12.java
@@ -1,6 +1,6 @@
 package com.Exercise.javaProgram.Array;
 
-//WAJP to find the largest number present in an Integer Array
+//WAJP to find the smallest number present in an Integer Array
 public class ArrayProgram12 {
 	public static void main(String[] args) {
 		int[] a= {7,14,25,22,74,5};
